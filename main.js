654600a955d0aa0c55a23b8a4b8e02a5af085025
Jacob Erisman <jacob.erisman@gmail.com>
2023-09-11 12:49:48 -0400

    Lab Completed - JE

--- main.js
+++ main.js
@@ -4,8 +4,33 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const emptyHearts = document.getElementsByClassName("like-glyph");
+Array.from(emptyHearts).forEach((emptyHeart) => {
+  emptyHeart.addEventListener('click', clickHandler);
+});
 
+function clickHandler () {
+  if (this.innerText === FULL_HEART){
+    this.innerText = EMPTY_HEART;
+    this.classList.remove('activated-heart');
+  } else {
+  mimicServerCall()
+  .then(() => {
+  this.innerText = FULL_HEART;
+  this.classList.add('activated-heart');
+  })
+  .catch((error) => {
+  const errorModal = document.getElementById('error-modal');
+  const errorMessage = errorModal.querySelector('.modal-message');
+  errorMessage.innerText = error;
 
+  errorModal.classList.remove('hidden');
+  setTimeout(() => {
+    errorModal.classList.add('hidden');
+  }, 3000)
+})
+}
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
